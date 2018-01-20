6e78c19a3b0881de7eb755440a8970f13f2327ac
Miha Susnik <mihec.susnik@gmail.com>
2018-01-20 19:14:59 +0100

    Forced full screen.

--- app/src/main/java/si/betoo/hodler/ui/base/BaseActivity.kt
+++ app/src/main/java/si/betoo/hodler/ui/base/BaseActivity.kt
@@ -3,6 +3,7 @@ package si.betoo.hodler.ui.base
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 import si.betoo.hodler.di.*
 import si.betoo.hodler.di.components.ActivityComponent
 import si.betoo.hodler.di.components.DaggerActivityComponent
@@ -23,6 +24,7 @@ abstract class BaseActivity : AppCompatActivity() {
                 .applicationComponent(BaseApplication.graph)
                 .build()
 
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
 }
