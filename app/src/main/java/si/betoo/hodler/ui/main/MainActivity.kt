ba266bb5ee6032243874877f902193ef4ae3d373
Miha Susnik <mihec.susnik@gmail.com>
2018-01-23 16:57:43 +0100

    Working on better main screen toolbar.

--- app/src/main/java/si/betoo/hodler/ui/main/MainActivity.kt
+++ app/src/main/java/si/betoo/hodler/ui/main/MainActivity.kt
@@ -1,16 +1,22 @@
 package si.betoo.hodler.ui.main
 
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
+import android.support.design.widget.CollapsingToolbarLayout
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.TextView
 import butterknife.ButterKnife
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
 import kotlinx.android.synthetic.main.activity_main.*
 import kotterknife.bindView
 import si.betoo.hodler.CurrencyFormatter
@@ -21,11 +27,11 @@ import si.betoo.hodler.ui.select.SelectCoinsActivity
 import si.betoo.hodler.ui.base.BaseActivity
 import si.betoo.hodler.ui.detail.CoinDetailActivity
 import si.betoo.hodler.ui.settings.SettingsActivity
+import timber.log.Timber
 import java.util.*
 import javax.inject.Inject
 
 class MainActivity : BaseActivity(), MainMVP.View {
-
 
     @Inject
     lateinit var presenter: MainMVP.Presenter
@@ -34,8 +40,11 @@ class MainActivity : BaseActivity(), MainMVP.View {
 
     //  Views
     private val recyclerView: RecyclerView by bindView(R.id.recycler_view)
+    private val appBarLayout: AppBarLayout by bindView(R.id.appbar)
+    private val collapsingToolbarLayout: CollapsingToolbarLayout by bindView(R.id.collapse_toolbar_layout)
 
     private val layoutTotal: ViewGroup by bindView(R.id.layout_total)
+    private val toolbar: Toolbar by bindView(R.id.toolbar)
 
     private val progress: ProgressBar by bindView(R.id.progress)
     private val progressTotal: ProgressBar by bindView(R.id.progress_total)
@@ -44,8 +53,9 @@ class MainActivity : BaseActivity(), MainMVP.View {
     private val textPercentChange: TextView by bindView(R.id.text_percent_change)
     private val textTotalChange: TextView by bindView(R.id.text_total_change)
 
+    private val toolbarText: TextView by bindView(R.id.toolbar_title)
+
     private val buttonRefresh: FloatingActionButton by bindView(R.id.button_refresh)
-    private val buttonSettings: ImageButton by bindView(R.id.button_settings)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,8 +73,36 @@ class MainActivity : BaseActivity(), MainMVP.View {
             presenter.refreshPrices()
         })
 
-        buttonSettings.setOnClickListener({
-            presenter.onSettingsClicked()
+        appBarLayout.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+
+            var isExpanded = true
+
+            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                val before = isExpanded
+
+                isExpanded = Math.abs(verticalOffset) <= toolbar.height
+
+                toolbar.post {
+                    if (before != isExpanded) {
+                        if (!isExpanded) {
+                            YoYo.with(Techniques.FadeIn)
+                                    .duration(200)
+                                    .playOn(toolbarText)
+
+                            toolbarText.visibility = View.VISIBLE
+                            layoutTotal.visibility = View.INVISIBLE
+
+                        } else {
+                            YoYo.with(Techniques.FadeIn)
+                                    .duration(200)
+                                    .playOn(layoutTotal)
+
+                            layoutTotal.visibility = View.VISIBLE
+                            toolbarText.visibility = View.INVISIBLE
+                        }
+                    }
+                }
+            }
         })
     }
 
@@ -102,6 +140,7 @@ class MainActivity : BaseActivity(), MainMVP.View {
         val formatter = CurrencyFormatter(currencyCode, currencySymbol)
 
         textHoldingValue.text = formatter.format(total)
+        toolbarText.text = formatter.format(total)
     }
 
     override fun showPercentChange(percentChange: Double) {
@@ -117,6 +156,9 @@ class MainActivity : BaseActivity(), MainMVP.View {
     }
 
     private fun setToolbarListeners(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+
         toolbar.setOnClickListener({
             presenter.switchCurrentCurrency()
         })
@@ -151,5 +193,23 @@ class MainActivity : BaseActivity(), MainMVP.View {
 
     override fun showSettings() {
         SettingsActivity.start(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_toolbar, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                presenter.onSettingsClicked()
+                true
+            }
+            else ->
+                super.onOptionsItemSelected(item)
+        }
     }
 }
